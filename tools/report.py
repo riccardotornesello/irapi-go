c099ff7e28d8116fb19b522168e60b11f084e35c
riccardo.tornesello <riccardo.tornesello@gmail.com>
2025-02-22 15:12:41 +0100

    generate endpoints report in markdown

--- tools/report.py
+++ tools/report.py
@@ -1,17 +1,25 @@
-import csv
 import json
 import os
+
+
+ok_emoji = "✅"
+error_emoji = "❌"
+null_emoji = "-"
 
 
 def generate_report():
     endpoints = json.load(open("output/endpoints.json"))
 
-    with open("output/report.csv", "w") as f:
-        writer = csv.writer(f)
-        writer.writerow(
-            ["Category", "Endpoint", "Has params", "Body parsed", "Has chunks"]
-        )
+    with open("output/report.md", "w") as f:
+
+        f.write("# Report\n\n")
+
+        f.write("## Endpoints\n\n")
+        f.write("| Category | Endpoint | Has params | Body parsed | Has chunks |\n")
+        f.write("|----------|----------|------------|-------------|------------|\n")
         for category, category_endpoints in endpoints.items():
+            if category == "meta":
+                continue
             for endpoint, endpoint_data in category_endpoints.items():
                 has_params = len(endpoint_data["parameters"]) > 0
                 has_chunks = os.path.exists(
@@ -20,6 +28,6 @@ def generate_report():
                 body_parsed = os.path.exists(
                     f"output/schemas/{category}__{endpoint}.{endpoint_data['format']}"
                 )
-                writer.writerow(
-                    [category, endpoint, has_params, body_parsed, has_chunks]
+                f.write(
+                    f"| {category} | {endpoint} | {ok_emoji if has_params else null_emoji} | {ok_emoji if body_parsed else null_emoji} | {ok_emoji if has_chunks else null_emoji} |\n"
                 )
